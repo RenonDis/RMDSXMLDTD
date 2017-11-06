b26badeb67133b094315db3bc37180ce4f8d240f
RenonDis <foobar@us.com>
2017-11-06 16:47:03 +0100

    Clearing validator

--- validator.py
+++ validator.py
@@ -4,9 +4,9 @@ import sys
 
 xml_file = sys.argv[1]
 
-# open and read xml file
-with open(xml_file, 'r') as xml_file_r:
-    xml_to_check = xml_file_r.read()
+#open and read xml file
+#with open(xml_file, 'r') as xml_file_r:
+#    xml_to_check = xml_file_r.read()
 
 # parse xml
 try:
@@ -15,8 +15,8 @@ try:
     print('XML well formed, syntax ok.')
 
 # check for file IO error
-#except IOError:
-#    print('Invalid File')
+except IOError:
+    print('Invalid File')
 
 # check for XML syntax errors
 except etree.XMLSyntaxError as err:
@@ -25,6 +25,6 @@ except etree.XMLSyntaxError as err:
         error_log_file.write(str(err.error_log))
     quit()
 
-#except:
-#    print('Unknown error, exiting.')
-#    quit()
+except:
+    print('Unknown error, exiting.')
+    quit()
